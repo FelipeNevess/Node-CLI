e06535b1eb1cb1d5e94e643aef7fa4e85592bb14
felipenevess <felipe012neves@gmail.com>
2022-08-13 18:45:01 -0300

    feat: import files utils

--- app/src/utils/index.ts
+++ app/src/utils/index.ts
@@ -1,4 +1,27 @@
-import { ReadFile } from './read';
-import { WriteFile } from './write';
+import { ExecSync } from '../utils/exec/exec';
+import { ExecCommands } from '../utils/exec';
 
-export { ReadFile, WriteFile };
+import { ReadFile } from './read';
+import { ReadFilePromises } from './read/read';
+
+import { WriteFile } from './write';
+import { WriteFilePromises } from './write/write';
+
+//////////////////////////////////////////
+// Uso da biblioteca readFile do Nodejs
+const readFilePromises = new ReadFilePromises();
+const readFile = new ReadFile(readFilePromises);
+
+//////////////////////////////////////////
+// Uso da biblioteca writeFile do Nodejs
+const writeFilePromises = new WriteFilePromises();
+const writeFile = new WriteFile(writeFilePromises);
+
+//////////////////////////////////////////
+// Uso da biblioteca ExecSync do Nodejs //
+const execSync = new ExecSync();
+const execCommands = new ExecCommands(execSync);
+
+////////////// Export files //////////////
+
+export { readFile, writeFile, execCommands };
