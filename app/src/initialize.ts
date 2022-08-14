6c0af8d0b54d59968e92aee88185f5315495d7d0
felipenevess <felipe012neves@gmail.com>
2022-08-14 14:27:48 -0300

    feat: adding terminal cleanup lib and decoration method

--- app/src/initialize.ts
+++ app/src/initialize.ts
@@ -1,8 +1,14 @@
+import clear from 'clear';
+
 import { Prompt, Prompts } from './data/prompt';
 import { IInitialize } from './interfaces';
+import { terminalDecoration } from './utils';
 
 class Initialize implements IInitialize {
   start(): void {
+    clear();
+    terminalDecoration.decorate('Node CLI');
+
     const prompt = new Prompts();
     new Prompt(prompt).execute();
   }
